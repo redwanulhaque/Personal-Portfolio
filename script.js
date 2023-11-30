d10abf436e810ba5b5a796e9e157a7035784fe56
Redwanul Haque <redwanulhaque703@gmail.com>
2023-11-29 22:14:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,381 @@
+// Website Tabs
+var content1 = document.getElementById("content1");
+var content2 = document.getElementById("content2");
+var content3 = document.getElementById("content3");
+var content4 = document.getElementById("content4");
+var content5 = document.getElementById("content5");
+var content6 = document.getElementById("content6");
+var content7 = document.getElementById("content7");
+
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
+var btn5 = document.getElementById("btn5");
+var btn6 = document.getElementById("btn6");
+var btn7 = document.getElementById("btn7");
+
+function open1(){
+  content1.style.transform = "translateX(0)";
+  content2.style.transform = "translateX(2500%)";
+  content3.style.transform = "translateX(900%)";
+  content4.style.transform = "translateX(900%)";
+  content5.style.transform = "translateX(1500%)";
+  content6.style.transform = "translateX(1500%)";
+  content7.style.transform = "translateX(900%)";
+
+  btn1.style.color = "#ff7846"
+  btn2.style.color = "#ccc"
+  btn3.style.color = "#ccc"
+  btn4.style.color = "#ccc"
+  btn5.style.color = "#ccc"
+  btn6.style.color = "#ccc"
+  btn7.style.color = "#ccc"
+
+  content1.style.transitionDelay = "0.3s";
+  content2.style.transitionDelay = "0s";
+  content3.style.transitionDelay = "0s";
+  content4.style.transitionDelay = "0s";
+  content5.style.transitionDelay = "0s";
+  content6.style.transitionDelay = "0s";
+  content7.style.transitionDelay = "0s";
+}
+
+function open2(){
+  content1.style.transform = "translateX(1500%)";
+  content2.style.transform = "translateX(0)";
+  content3.style.transform = "translateX(900%)";
+  content4.style.transform = "translateX(900%)";
+  content5.style.transform = "translateX(1500%)";
+  content6.style.transform = "translateX(1500%)";
+  content7.style.transform = "translateX(900%)";
+
+  btn2.style.color = "#ff7846"
+  btn1.style.color = "#ccc"
+  btn3.style.color = "#ccc"
+  btn4.style.color = "#ccc"
+  btn5.style.color = "#ccc"
+  btn6.style.color = "#ccc"
+  btn7.style.color = "#ccc"
+
+  content2.style.transitionDelay = "0.3s";
+  content1.style.transitionDelay = "0s";
+  content3.style.transitionDelay = "0s";
+  content4.style.transitionDelay = "0s";
+  content5.style.transitionDelay = "0s";
+  content6.style.transitionDelay = "0s";
+  content7.style.transitionDelay = "0s";
+}
+
+function open3(){
+  content1.style.transform = "translateX(1500%)";
+  content2.style.transform = "translateX(2500%)";
+  content3.style.transform = "translateX(0)";
+  content4.style.transform = "translateX(900%)";
+  content5.style.transform = "translateX(1500%)";
+  content6.style.transform = "translateX(1500%)";
+  content7.style.transform = "translateX(900%)";
+
+  btn3.style.color = "#ff7846"
+  btn2.style.color = "#ccc"
+  btn1.style.color = "#ccc"
+  btn4.style.color = "#ccc"
+  btn5.style.color = "#ccc"
+  btn6.style.color = "#ccc"
+  btn7.style.color = "#ccc"
+
+  content3.style.transitionDelay = "0.3s";
+  content2.style.transitionDelay = "0s";
+  content1.style.transitionDelay = "0s";
+  content4.style.transitionDelay = "0s";
+  content5.style.transitionDelay = "0s";
+  content6.style.transitionDelay = "0s";
+  content7.style.transitionDelay = "0s";
+}
+
+function open4(){
+  content1.style.transform = "translateX(1500%)";
+  content2.style.transform = "translateX(2500%)";
+  content3.style.transform = "translateX(900%)";
+  content4.style.transform = "translateX(0)";
+  content5.style.transform = "translateX(1500%)";
+  content6.style.transform = "translateX(1500%)";
+  content7.style.transform = "translateX(900%)";
+
+  btn4.style.color = "#ff7846"
+  btn2.style.color = "#ccc"
+  btn3.style.color = "#ccc"
+  btn1.style.color = "#ccc"
+  btn5.style.color = "#ccc"
+  btn6.style.color = "#ccc"
+  btn7.style.color = "#ccc"
+
+  content4.style.transitionDelay = "0.3s";
+  content2.style.transitionDelay = "0s";
+  content3.style.transitionDelay = "0s";
+  content1.style.transitionDelay = "0s";
+  content5.style.transitionDelay = "0s";
+  content6.style.transitionDelay = "0s";
+  content7.style.transitionDelay = "0s";
+}
+
+function open5(){
+  content1.style.transform = "translateX(1500%)";
+  content2.style.transform = "translateX(2500%)";
+  content3.style.transform = "translateX(900%)";
+  content4.style.transform = "translateX(900%)";
+  content5.style.transform = "translateX(0)";
+  content6.style.transform = "translateX(1500%)";
+  content7.style.transform = "translateX(900%)";
+
+  btn5.style.color = "#ff7846"
+  btn2.style.color = "#ccc"
+  btn3.style.color = "#ccc"
+  btn4.style.color = "#ccc"
+  btn1.style.color = "#ccc"
+  btn6.style.color = "#ccc"
+  btn7.style.color = "#ccc"
+
+  content5.style.transitionDelay = "0.3s";
+  content2.style.transitionDelay = "0s";
+  content3.style.transitionDelay = "0s";
+  content4.style.transitionDelay = "0s";
+  content1.style.transitionDelay = "0s";
+  content6.style.transitionDelay = "0s";
+  content7.style.transitionDelay = "0s";
+}
+
+function open6(){
+  content1.style.transform = "translateX(1500%)";
+  content2.style.transform = "translateX(2500%)";
+  content3.style.transform = "translateX(900%)";
+  content4.style.transform = "translateX(900%)";
+  content5.style.transform = "translateX(1500%)";
+  content6.style.transform = "translateX(0)";
+  content7.style.transform = "translateX(900%)";
+
+  btn6.style.color = "#ff7846"
+  btn2.style.color = "#ccc"
+  btn3.style.color = "#ccc"
+  btn4.style.color = "#ccc"
+  btn5.style.color = "#ccc"
+  btn1.style.color = "#ccc"
+  btn7.style.color = "#ccc"
+
+  content6.style.transitionDelay = "0.3s";
+  content2.style.transitionDelay = "0s";
+  content3.style.transitionDelay = "0s";
+  content4.style.transitionDelay = "0s";
+  content5.style.transitionDelay = "0s";
+  content1.style.transitionDelay = "0s";
+  content7.style.transitionDelay = "0s";
+}
+
+function open7(){
+  content1.style.transform = "translateX(1500%)";
+  content2.style.transform = "translateX(2500%)";
+  content3.style.transform = "translateX(900%)";
+  content4.style.transform = "translateX(900%)";
+  content5.style.transform = "translateX(1500%)";
+  content6.style.transform = "translateX(1500%)";
+  content7.style.transform = "translateX(0)";
+
+  btn7.style.color = "#ff7846"
+  btn2.style.color = "#ccc"
+  btn3.style.color = "#ccc"
+  btn4.style.color = "#ccc"
+  btn5.style.color = "#ccc"
+  btn6.style.color = "#ccc"
+  btn1.style.color = "#ccc"
+
+  content7.style.transitionDelay = "0.3s";
+  content2.style.transitionDelay = "0s";
+  content3.style.transitionDelay = "0s";
+  content4.style.transitionDelay = "0s";
+  content5.style.transitionDelay = "0s";
+  content6.style.transitionDelay = "0s";
+  content1.style.transitionDelay = "0s";
+}
+
+
+
+
+// personal Projects
+var pages = document.getElementsByClassName('page');
+for(var i = 0; i < pages.length; i++)
+  {
+    var page = pages[i];
+    if (i % 2 === 0)
+      {
+        page.style.zIndex = (pages.length - i);
+      }
+  }
+
+document.addEventListener('DOMContentLoaded', function(){
+  for(var i = 0; i < pages.length; i++)
+    {
+      pages[i].pageNum = i + 1;
+      pages[i].onclick=function()
+        {
+          if (this.pageNum % 2 === 0)
+            {
+              this.classList.remove('flipped');
+              this.previousElementSibling.classList.remove('flipped');
+            }
+          else
+            {
+              this.classList.add('flipped');
+              this.nextElementSibling.classList.add('flipped');
+            }
+         }
+      }
+})
+
+
+
+
+// Countdown Timer
+(function () {
+  const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      nextYear = yyyy + 1,
+      dayMonth = "05/21/",
+      birthday = dayMonth + yyyy;
+
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+
+  const countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {
+
+        const now = new Date().getTime(),
+              distance = countDown - now;
+
+        document.getElementById("days").innerText = "0",
+          document.getElementById("hours").innerText = "0",
+          document.getElementById("minutes").innerText = "0",
+          document.getElementById("seconds").innerText =  "0";
+
+        if (distance < 0) {
+          document.getElementById("headline").innerText = "It's my birthday!";
+          document.getElementById("countdown").style.display = "none";
+          document.getElementById("content").style.display = "block";
+          clearInterval(x);
+        }
+      }, 0)
+}());
+
+
+
+
+// Education
+var p = $('p');
+p.each(function(){
+    var t = $(this).text().replace(/\s/g, unescape('%a0')); /* Spaces collapse, so make them non-breaking */
+    var o = '';
+    for(var i = 0; i< t.length; i++){
+        o += '<span class="normal">' + t[i] + '</span>';
+    }
+    $(this).html(o);
+});
+
+function lift(){
+	$('.fallen').removeClass('fallen').addClass('normal');
+	window.setTimeout(drop, 60);
+}
+
+function drop(){
+  var s = $('span.normal');
+  if (s.length == 0){
+   window.setTimeout(lift, 10000);
+   return;
+  }
+
+  s.eq(Math.floor(Math.random() *   s.length)).addClass('fallen').removeClass('normal');
+	window.setTimeout(drop, 60);
+}
+
+drop();
+
+
+
+
+// Courses List
+function myFunction() {
+  var csci313 = document.getElementById("myPopup");
+  csci313.classList.toggle("show");
+}
+
+function myFunction2() {
+  var csci348 = document.getElementById("myPopup2");
+  csci348.classList.toggle("show2");
+}
+
+function myFunction3() {
+  var csci323 = document.getElementById("myPopup3");
+  csci323.classList.toggle("show3");
+}
+
+function myFunction4() {
+  var csci381 = document.getElementById("myPopup4");
+  csci381.classList.toggle("show4");
+}
+
+function myFunction5() {
+  var math242 = document.getElementById("myPopup5");
+  math242.classList.toggle("show5");
+}
+
+function myFunction6() {
+  var csci340 = document.getElementById("myPopup6");
+  csci340.classList.toggle("show6");
+}
+
+function myFunction7() {
+  var csci316 = document.getElementById("myPopup7");
+  csci316.classList.toggle("show7");
+}
+
+function myFunction8() {
+  var csci355 = document.getElementById("myPopup8");
+  csci355.classList.toggle("show8");
+}
+
+function myFunction9() {
+  var csci3811 = document.getElementById("myPopup9");
+  csci3811.classList.toggle("show9");
+}
+
+function myFunction10() {
+  var csci370 = document.getElementById("myPopup10");
+  csci370.classList.toggle("show10");
+}
+
+function myFunction11() {
+  var csci331 = document.getElementById("myPopup11");
+  csci331.classList.toggle("show11");
+}
+
+function myFunction12() {
+  var csci38111 = document.getElementById("myPopup12");
+  csci38111.classList.toggle("show12");
+}
+
+function myFunction13() {
+  var math250 = document.getElementById("myPopup13");
+  math250.classList.toggle("show13");
+}
+
+function myFunction14() {
+  var csci381111 = document.getElementById("myPopup14");
+  csci381111.classList.toggle("show14");
+}
